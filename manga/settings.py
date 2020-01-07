cb5e490a66194625e877292a71aca7237971a740
eclipsium <eclipsium@yandex.ru>
2020-01-08 02:44:18 +0700

    Deploy beta

--- manga/settings.py
+++ manga/settings.py
@@ -121,7 +121,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 # ]
 
 ROOT_URLCONF = 'manga.urls'
-FRONTEND_DIR = os.path.join(BASE_DIR, 'frontend')
+FRONTEND_DIR = os.path.join(BASE_DIR, 'frontend/')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
@@ -159,12 +159,21 @@ WSGI_APPLICATION = 'manga.wsgi.application'
 
 DATABASES = {
     'default': {
+        #windows
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'manga_db',
+        # 'USER': 'manga_admin',
+        # 'PASSWORD': 'mangareader',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
+        #ubuntu
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'manga_db',
-        'USER': 'manga_admin',
-        'PASSWORD': 'mangareader',
+        'USER': 'manga',
+        'PASSWORD': 'manga_password11',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
