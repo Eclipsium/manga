76cdd24bd350535e4c51877cab19c7cc24122a12
Eclipsium <Eclipsium@yandex.ru>
2020-01-11 21:18:20 +0700

    change setting to server prod

--- apps/manga_api/celery.py
+++ apps/manga_api/celery.py
@@ -6,7 +6,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manga.settings')
 os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
 
 app = Celery('manga_api',
-             broker='amqp://manga:mangapassword@localhost:5672/mangahost',
+             broker='redis://localhost:6379/0',
              )
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
