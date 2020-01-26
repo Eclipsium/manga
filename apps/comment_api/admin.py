049cdd06377779cb06f00c845c86f43196bd6346
Eclipsium <Eclipsium@yandex.ru>
2020-01-26 23:08:36 +0700

    Fix view page. Complete comments api

--- apps/comment_api/admin.py
+++ apps/comment_api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.comment_api.models import Comments
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    pass
